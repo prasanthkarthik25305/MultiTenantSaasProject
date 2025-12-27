3f1b10cd228416402ac6d0ea381c3940aa118715
prasanthkarthik25305 <22p31a0564@acet.ac.in>
2025-12-27 11:15:47 +0530

    knefile is configured

---
+++ backend/knexfile.js
@@ -0,0 +1,18 @@
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      port: 5432
+    },
+    migrations: {
+      directory: "./migrations"
+    },
+    seeds: {
+      directory: "./seeds"
+    }
+  }
+};
